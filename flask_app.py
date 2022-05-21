5b2bfc2701d91d62b125a798bec2c40bb0152a28
tecnocristi21 <a27765@aefranciscosanches.pt>
2022-05-21 10:35:57 +0100

    Check update data and add delete data

--- flask_app.py
+++ flask_app.py
@@ -9,7 +9,7 @@ app.secret_key=b'263433102'
 bootstrap=Bootstrap(app) #desenvolver componentes de interface e front-end para sites ligado ambem com a pare de beleza do site
 
 @app.route("/")
-def template_test():
+def home_page():
     sqlite_connection=sqlite3.connect('shop.db')
     cursor=sqlite_connection.cursor()
     data=cursor.execute("Select * from Product")
@@ -34,15 +34,54 @@ def add_data():
         sqlite_connection.commit()
         sqlite_connection.close()
         flash('Row successfully added!','success')
-        return redirect(url_for('template_test'))
+        return redirect(url_for('home_page'))
     return render_template('add_data.html',title='Add Data', form=form)
 
 
-@app.route("/edit_data/<int:product_id>")
+@app.route("/edit_data/<product_id>",methods=['GET','POST'])
 def edit_data(product_id):
     form=Add_Form(request.form)
-    form.product_ID=product_id
+    if request.method=='GET':
+        sqlite_connection=sqlite3.connect('shop.db')
+        cursor = sqlite_connection.cursor()
+        database_string='''SELECT * FROM Product WHERE Product_ID="{id}";'''.format(id=product_id)
+        data=cursor.execute(database_string)
+        for product in data:
+            form.product_ID=product[0]
+            form.list_ID.data=product[1]
+            form.prodName.data=product[2]
+            form.date_sold.data=product[3]
+            form.descript.data=product[4]
+        cursor.close()
+        sqlite_connection.close()
+    if request.method=='POST':
+        form_list_ID=form.list_ID.data
+        form_prodName=form.prodName.data
+        form_date_sold=form.date_sold.data
+        form_descript=form.descript.data
+        #print(form_list_ID,form_prodName,form_date_sold,form_descript)
+        sqlite_connection = sqlite3.connect('shop.db')
+        cursor = sqlite_connection.cursor()
+        sql_command='''UPDATE Product SET product_List_ID='{List}', product_Name='{Name}', date_Sold='{sold}',description='{descript}' WHERE Product_ID="{id}"'''\
+            .format(id=product_id,List=form_list_ID,Name=form_prodName,sold=form_date_sold,descript=form_descript)
+        cursor.execute(sql_command)
+        sqlite_connection.commit()
+        sqlite_connection.close()
+        flash('Row successfully edited!','success')
+        return redirect(url_for('home_page'))
     return render_template('add_data.html',title='Edit data',form=form)
+
+
+@app.route("/delete_data/<product_id>")
+def delete_data(product_id):
+    sqlite_connection = sqlite3.connect('shop.db')
+    cursor = sqlite_connection.cursor()
+    database_string = '''DELETE FROM Product WHERE Product_ID="{id}";'''.format(id=product_id)
+    cursor.execute(database_string)
+    sqlite_connection.commit()
+    sqlite_connection.close()
+    flash('Row successfully deleted!', 'success')
+    return redirect(url_for('home_page'))
 
 
 if __name__ == '__main__':
